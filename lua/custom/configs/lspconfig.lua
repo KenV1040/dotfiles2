4a25f25dade4d02688a63f831c7b7ff6d34dd40a
ken vu <kenvu@protonmail.com>
2023-12-26 18:11:25 +0000

    Updated pyright config for venv support

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -5,6 +5,18 @@ local capabilities = base.capabilities
 local lspconfig = require("lspconfig")
 
 local servers = {"pyright"}
+local pathValue = os.getenv("PATH")
+
+-- Check if the PATH contains a pattern indicating a Python venv
+local isVenvActivated = string.match(pathValue, "/venv/bin") or string.match(pathValue, "\\.venv\\Scripts")
+
+-- Check the result of the match and print accordingly
+if isVenvActivated then
+  print("Python virtual environment seems to be activated.")
+  vim.g.pyright_python_binary = pathValue .. "/python"
+else
+    print("Python virtual environment is not activated.")
+end
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup({
